d38f6295f7d3ad8d80d135b20a5867c1e7e81ddd
Caio-Santana <caio.santana@live.com>
2023-07-11 18:00:18 -0300

    First commit

---
+++ src/main/java/com/samsoftware/bandhub/BandhubApplication.java
@@ -0,0 +1,13 @@
+package com.samsoftware.bandhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samsoftware/bandhub/BandhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsoftware.bandhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
